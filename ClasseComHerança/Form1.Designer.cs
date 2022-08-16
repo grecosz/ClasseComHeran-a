47d9c04ac26beb161e270ec557315c3f50aec841
Mariana Greco Mantovani <94694482+grecosz@users.noreply.github.com>
2022-08-16 11:28:35 -0300

    Projeto Base

---
+++ ClasseComHerança/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ClasseComHerança
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(67, 204);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(179, 95);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Cadastrar Professor";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(276, 204);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(179, 95);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "Cadastrar Aluno";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(485, 204);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(179, 95);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "Sair";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(745, 521);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+    }
+}
+
---
+++ ClasseComHerança/Pessoa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseComHerança
+{
+    public abstract class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+        // Método construtor sem parâmetro
+        public Pessoa()
+        {
+            this.nome = null;
+            this.idade = 0;
+        }
+
+        // Construtor com parâmetro
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        // Propriedades dos atributos para acesso
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        // Método da classe ImprimeDados
+        // public abstract String ImprimeDados();
+
+    }   
+        
+}
